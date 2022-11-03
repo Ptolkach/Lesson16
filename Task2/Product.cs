36a549361b6bc0238e8b35f336f916baea882126
Pavel Tolkach <pb.tolkach@outlook.com>
2022-11-03 10:11:04 +0300

    Добавьте файлы проекта.

---
+++ Task2/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
